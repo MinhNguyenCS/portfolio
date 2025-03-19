176472acec69c20532d3c6ce60a066cd00fa6e0d
Minh Khang Nguyen <khangminhnguyengl@gmail.com>
2025-03-18 22:41:05 -0700

    first commit

---
+++ roleScript.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "a Backend Developer",
+      "a Software Engineer",
+      "Minh Khang Nguyen",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
